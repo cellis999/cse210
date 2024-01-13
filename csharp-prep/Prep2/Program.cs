cf907cec358d4743babc1c1a77bfd7d32a51f172
Connor <wconnorellis@gmail.com>
2024-01-12 20:16:27 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = null;
+        string passClass = null;
+        Console.Write("Enter Grade Percent: ");
+        string gradePercentText = Console.ReadLine();
+        int gradePercent = int.Parse(gradePercentText);
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent < 90 && gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent < 80 && gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent < 70 && gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (gradePercent >= 70)
+        {
+            passClass = "You pass the class!";
+        }
+        else
+        {
+            passClass = "Try again next semester.";
+        }
+        Console.WriteLine($"{letter}");
+        Console.Write($"{passClass}");
     }
 }
