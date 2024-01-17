09c9c7cf24c5b5b52e8546a1940cdfac0a0c9316
Connor <wconnorellis@gmail.com>
2024-01-17 10:48:55 -0700

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        bool numberGuessed = false;
+
+        while (numberGuessed == false)
+        {
+            Console.Write("What is your guess? ");
+            string readGuess = Console.ReadLine();
+            int guess = int.Parse(readGuess);
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                numberGuessed = true;
+            }
+        }
+
     }
 }
