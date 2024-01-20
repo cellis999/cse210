dc5ab4cb371f12fa8b25bea4ce3d8f4987cd239b
Connor <wconnorellis@gmail.com>
2024-01-20 00:15:11 -0700

    Finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        bool done = false;
+        int sum = 0;
+        int largest = 0;
+        List<int> numbers = new List<int>();
+        while (done == false)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+            if (number == 0)
+            {
+                done = true;
+            }
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        float average = sum / (numbers.Count - 1);
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largest}");
     }
 }
