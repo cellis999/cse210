619b3f7e2308557c973c133936971e303c4a27ca
Connor <wconnorellis@gmail.com>
2024-01-24 21:55:03 -0700

    Finished prep

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,11 @@
+public class Job
+{
+    public string company;
+    public string jobTitle;
+    public int startYear;
+    public int endYear;
+    public void Display()
+    {
+        Console.WriteLine($"{jobTitle} ({company}) {startYear}-{endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1.company = "Microsoft";
+        job1.jobTitle = "Software Engineer";
+        job1.startYear = 2019;
+        job1.endYear = 2022;
+
+        Job job2 = new Job();
+        job2.company = "Apple";
+        job2.jobTitle = "Manager";
+        job2.startYear = 2022;
+        job2.endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume.name = "Allison Rose";
+
+        myResume.jobs.Add(job1);
+        myResume.jobs.Add(job2);
+
+        myResume.Display();
     }
 }
